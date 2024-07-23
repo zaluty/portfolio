929d6e46e1186629a20d39486eff408b307893d6
hamza <hamzaredone6@gmail.com>
2024-07-23 15:07:23 +0100

    email copy

--- app/animations/AnimatedBody.tsx
+++ app/animations/AnimatedBody.tsx
@@ -6,57 +6,59 @@ type AnimatedBodyProps = {
   text: string;
   className?: string;
   delay?: number;
+  onClick?: () => void;
 };
 
 export default function AnimatedBody({
-    text,
-    className,
-    delay,
+  text,
+  className,
+  delay,
+  onClick,
 }: AnimatedBodyProps) {
-    const ctrls = useAnimation();
+  const ctrls = useAnimation();
 
-    const { ref, inView } = useInView({
-        threshold: 0.1,
-        triggerOnce: false,
-    });
+  const { ref, inView } = useInView({
+    threshold: 0.1,
+    triggerOnce: false,
+  });
 
-    useEffect(() => {
-        if (inView) {
-            ctrls.start("visible");
-        }
-        if (!inView) {
-            ctrls.start("hidden");
-        }
-    }, [ctrls, inView]);
+  useEffect(() => {
+    if (inView) {
+      ctrls.start("visible");
+    }
+    if (!inView) {
+      ctrls.start("hidden");
+    }
+  }, [ctrls, inView]);
 
-    const bodyAnimation = {
-        hidden: {
-            opacity: 0,
-            y: `1em`,
-        },
-        visible: {
-            opacity: 1,
-            y: `0em`,
-            transition: {
-                delay: delay,
-                duration: 1,
-                ease: [0.2, 0.65, 0.3, 0.9],
-            },
-        },
-    };
+  const bodyAnimation = {
+    hidden: {
+      opacity: 0,
+      y: `1em`,
+    },
+    visible: {
+      opacity: 1,
+      y: `0em`,
+      transition: {
+        delay: delay,
+        duration: 1,
+        ease: [0.2, 0.65, 0.3, 0.9],
+      },
+    },
+  };
 
-    return (
-        <motion.p
-            aria-label={text}
-            role="heading"
-            className={className}
-            ref={ref}
-            aria-hidden="true"
-            initial="hidden"
-            animate={ctrls}
-            variants={bodyAnimation}
-        >
-            {text}
-        </motion.p>
-    );
+  return (
+    <motion.p
+      aria-label={text}
+      role="heading"
+      className={className}
+      ref={ref}
+      aria-hidden="true"
+      initial="hidden"
+      animate={ctrls}
+      variants={bodyAnimation}
+    >
+      {text}
+    </motion.p>
+  );
 }
--- app/sections/Contact.tsx
+++ app/sections/Contact.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 import { inter } from "../fonts/inter";
 import "../animations/animate.css";
@@ -6,9 +7,15 @@ import AnimatedTitle from "../animations/AnimatedTitle";
 import AnimatedWords from "../animations/AnimatedWords";
 import { motion } from "framer-motion";
 import ContactBackground from "../components/background/ContactBackground";
-import React from "react";
+import React, { useState } from "react";
+import { toast } from "sonner";
 
 const Contact = () => {
+  const copyEmail = () => {
+    navigator.clipboard.writeText("mugnumdev@gmail.com");
+
+    toast.success("Email copied to clipboard");
+  };
   return (
     <motion.section
       className="relative z-10 flex h-[85vh] w-full items-center justify-center overflow-hidden py-16 md:h-[80vh] md:py-20 lg:h-[90vh] lg:pt-0 lg:pb-28"
@@ -39,14 +46,15 @@ const Contact = () => {
                 "-mb-1 inline-block overflow-hidden pt-1 sm:-mb-2 md:-mb-3 lg:-mb-4"
               }
             />
-            <Link
-              href="mailto:karthikmudunuri999@gmail.com?subject=Lets%20work%20together!&amp;body=Hello%2C%20I%20think%20we%20need%20you%20to%20work%20on%2Fcollaborate%20this%20particular%20product...%20Reach%20out%20as%20soon%20as%20you%20can.By%20eldoraui"
-              target="_blank"
-              aria-label="Send me an email"
-              className="mt-1 w-[147px] flex-1 underline underline-offset-2 hover:no-underline sm:mt-2 sm:w-[147px] md:mt-3 md:w-[170px] lg:mt-4"
+            <span
+              onClick={copyEmail}
+              className="mt-1 w-[147px] flex-1 cursor-copy  underline  underline-offset-2 hover:no-underline sm:mt-2 sm:w-[147px] md:mt-3 md:w-[170px] lg:mt-4"
             >
-              <AnimatedBody text={"Send me an email"} className={""} />
-            </Link>
+              <AnimatedBody
+                text={"Send me an email"}
+                className="  cursor-copy"
+              />
+            </span>
           </div>
 
           <div className="flex gap-10 text-[16px] font-bold text-[#e4ded7]  sm:gap-14 sm:text-[24px] md:gap-10 md:text-[16px] lg:gap-20 lg:text-[28px]">
@@ -58,7 +66,7 @@ const Contact = () => {
               <AnimatedTitle
                 text={"GITHUB"}
                 className={
-                  "text-[16px] font-bold text-[#e4ded7] sm:text-[20px] md:text-[16px] lg:text-[28px]"
+                  "text-[16px] font-bold text-[#e4ded7]  sm:text-[20px] md:text-[16px] lg:text-[28px]"
                 }
                 wordSpace={"mr-[0.25em]"}
                 charSpace={"mr-[0.01em]"}
